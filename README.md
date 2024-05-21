ed6727e62ae191fed075acd2ea6251c798df7dd5
Aomzz <anusak.k@mail.bbvc.ac.th>
2024-05-21 09:46:47 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# TypeScript MERN E-Commerce
+
+## Lesson
+1. Introduction
+2. Install Tools 
+3 Create TypeScript
+4. Create Git
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+  return (
+    <>
+      <header>Ajarn Ohm</header>
+      <main>Content</main>
+      <footer>All right reserved</footer>
+    </>
+  )
+}
+
+export default App
